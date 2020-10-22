9b841b555bb9f64b40c46a33484da3eddaf49716
barispunar <39291650+barispunar@users.noreply.github.com>
2020-10-22 14:55:29 +0300

    Add project files.

---
+++ konusarak_ogreniyorum/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace konusarak_ogreniyorum.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
